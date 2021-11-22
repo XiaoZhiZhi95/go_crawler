986d9d611c9f726c1efe1f10682002db206a2432
fangzhi <842938617@qq.com>
2021-11-22 15:47:46 +0800

    simple template

--- testTemplate/main.go
+++ testTemplate/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"html/template"
+	"net/http"
+)
+
+// testTemplate
+/* @Description: 模版
+*  @param w
+*  @param r
+*/
+func testTemplate(w http.ResponseWriter, r *http.Request)  {
+	//解析模版
+	//t, _ := template.ParseFiles("./index.html")
+	//执行模版
+	//t.Execute(w, "Hello lili")
+
+	t := template.Must(template.ParseFiles("./index.html"))
+	t.Execute(w, "hello lili")
+}
+
 func main() {
-	$END$
+	http.HandleFunc("/testTemplate", testTemplate)
+	http.ListenAndServe(":8080", nil)
 }
